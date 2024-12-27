12e4d416b24944f1d6fdd34aee46a767efe12072
Suyogya Luitel <lsuyogya@gmail.com>
2024-12-27 10:41:54 +0545

    init homepage

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        biryani: ['Biryani', 'sans-serif'],
+        unna: ['Unna', 'serif'],
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      green: colors.green,
+      primary: '#f1e7a3',
+      secondary: '#9b51b4',
+      customGray: '#323032',
+      light: '#f7f4f0',
+    },
+  },
+  safelist: [
+    'fixed',
+    'top-0',
+    'translate-y-0',
+    '-translate-y-full',
+    'shadow-md',
+  ],
+  plugins: [require('tailwindcss-motion')],
+};
